b9d3aec3cad0b47ade32a2b53bd6707d20939e02
Jorge Lobo <jorge-lobo@outlook.pt>
2024-04-19 00:54:37 +0100

    - add splashScreen

--- app/src/main/java/com/example/euro24/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/euro24/ui/splash/SplashScreenActivity.kt
@@ -1,21 +1,27 @@
 package com.example.euro24.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.euro24.R
+import androidx.lifecycle.lifecycleScope
+import com.example.euro24.databinding.ActivitySplashScreenBinding
+import com.example.euro24.ui.main.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
         }
     }
 }
