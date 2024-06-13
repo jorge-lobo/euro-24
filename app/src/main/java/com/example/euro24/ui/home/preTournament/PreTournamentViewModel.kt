cc12cf6f912abb6992918648fa7460a3d1362dc8
Jorge Lobo <jorge-lobo@outlook.pt>
2024-06-13 12:43:58 +0100

    fix(PreTournamentViewModel): fix `countDaysToTournament` method to calculate days correctly
    - Correct the calculation of the days difference between today's date and the pre-tournament date.
    - Ensure both dates are set to the start of the day to avoid time discrepancies.

--- app/src/main/java/com/example/euro24/ui/home/preTournament/PreTournamentViewModel.kt
+++ app/src/main/java/com/example/euro24/ui/home/preTournament/PreTournamentViewModel.kt
@@ -3,11 +3,28 @@ package com.example.euro24.ui.home.preTournament
 import android.app.Application
 import com.example.euro24.ui.common.BaseViewModel
 import com.example.euro24.utils.DateUtils
+import java.util.Calendar
 
 class PreTournamentViewModel(application: Application) : BaseViewModel(application) {
 
     fun countDaysToTournament(): Int {
-        val differenceInMillis = (DateUtils.datePreTournament?.time ?: 0) - DateUtils.currentDate.time
+        val startOfToday = Calendar.getInstance().apply {
+            time = DateUtils.currentDate
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.time
+
+        val startOfPreTournament = Calendar.getInstance().apply {
+            time = DateUtils.datePreTournament!!
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.time
+
+        val differenceInMillis = startOfPreTournament.time - startOfToday.time
         val differenceInDays = differenceInMillis / (1000 * 60 * 60 * 24)
         return differenceInDays.toInt()
     }
